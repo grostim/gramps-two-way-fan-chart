f47a0438bd225df955b06621cad1405243068753
Timothee Gros <timothee.gros@gmail.com>
2026-09-20 18:16:59 +0200

    chore: advance addon version to 1.2.89

--- TwoWayFanChart/TwoWayFanChart.gpr.py
+++ TwoWayFanChart/TwoWayFanChart.gpr.py
@@ -10,7 +10,7 @@ register(
         "and portraits around a center family."
     ),
 
-    version="1.2.88",
+    version="1.2.89",
     gramps_target_version="6.0",
     status=STABLE,
     fname="TwoWayFanChart.py",
